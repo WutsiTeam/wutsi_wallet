4be3e7f8bd6e2ce5f7a1e7dd9def0ef5730f882d
wutsi <herve.tchepannou@wutsi.com>
2021-12-06 13:21:10 -0500

    update camera

--- lib/src/camera.dart
+++ lib/src/camera.dart
@@ -1,7 +1,26 @@
 import 'package:camera/camera.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:sdui/sdui.dart';
 
 Future<int> initCamera() async {
-  sduiCameras = await availableCameras();
-  return sduiCameras.length;
+  bool granted = await _requestCameraPermission();
+  if (granted) {
+    sduiCameras = await availableCameras();
+    return sduiCameras.length;
+  } else {
+    return 0;
+  }
+}
+
+Future<bool> _requestCameraPermission() async {
+  try {
+    var status = await Permission.camera.status;
+    if (status.isDenied) {
+      return await Permission.camera.request().isGranted;
+    } else {
+      return true;
+    }
+  } catch (e) {
+    return true;
+  }
 }
